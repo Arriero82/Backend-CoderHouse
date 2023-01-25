6acd1f0a266a7ccb9f4c30310a459086f60bc3ee
Arriero82 <gabriel.vulliud@gmail.com>
2023-01-25 15:25:57 -0300

    add socket.io, handlebars and 2 new routes

---
+++ entrega-clase-10/src/public/js/index.js
@@ -0,0 +1,27 @@
+console.log("on");
+
+const socket = io();
+
+const cardsContainer = document.getElementById("cards-container");
+
+socket.on("message", (data) => {
+  console.log(data);
+});
+
+const render = (array) => {
+  const html = array
+    .map((elem) => {
+      return `<div class="cards">
+            <p>Artículo: ${elem.title}</p>
+            <p>Descripción: ${elem.description}</p>
+            <p>Precio: $ ${elem.price}</p>
+            <img src="${elem.thumbnails}">
+            </div >`;
+    });
+  document.querySelector("#cards-container").innerHTML = html;
+};
+
+socket.on("message", (data) => {
+  //console.log(data);
+  render(data);
+});
